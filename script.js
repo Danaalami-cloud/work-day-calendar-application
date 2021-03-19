5ef74353a68a5e8a76c7a978a0a7a02c6d42fd48
Dana Alami <dana.alami18@gmail.com>
2021-03-19 14:38:07 +0200

    added local storage

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
 }
 setInterval(displayTime,1000);
+
+function timeBlock() {
+    var pressntTime = this.textContent;
+    if (timeBlock < timeNow ) {
+       return.setAttribute("style", "color:white;");
+    }
+    else if (timeBlock === timeNow) {
+
+    }
+    else (timeBlock > timeNow) {
+
+    }
+}
+
+function saveInfo () {
+    var storeInfo = JSON.parse(window.localStorage.getItem("info")) || [];
+    storeInfo.push(savedInfo);
+    window.localStorage.setItem("info", JSON.stringify(saveInfo));
+}
