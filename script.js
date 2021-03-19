87a723f336ba4b191690903228f1ab14aeeeab22
Dana Alami <dana.alami18@gmail.com>
2021-03-19 18:21:50 +0200

    tidying up the local storage in js

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-var timeDisplayEl = $('#time-display');
-var saveButtonEl = $('.saveBtn');
-var textEl = $('.description');
+var timeDisplayEl = $("#time-display");
+var textEl = $(".description");
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] h:mm:ss a');
-    timeDisplayEl.text(rightNow);
+  var rightNow = moment().format("MMM DD, YYYY [at] h:mm:ss a");
+  timeDisplayEl.text(rightNow);
 }
-setInterval(displayTime,1000);
+
+$(".saveBtn").on("click", function () {
+  var dailyTask = $(this).siblings(".description").val();
+  var taskTime = $(this).parent().attr("id");
+  localStorage.setItem(taskTime, dailyTask);
+});
+
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 function colorTime() {
-    var presentTime = this.textContent;
-    if (colorTime < timeNow ) {
-       return ("style", "color:white;");
-    
-    }
-    else if (colorTime === timeNow) {
-       setAttribute("style", "color:red;");
-    }
-    else (colorTime > timeNow); {
-
-    }
+  var presentTime = this.textContent;
+  if (colorTime < timeNow) {
+    return "style", "color:white;";
+  } else if (colorTime === timeNow) {
+    setAttribute("style", "color:red;");
+  } else colorTime > timeNow;
+  {
+  }
 }
 
-function saveInfo () {
-    var storeInfo = JSON.parse(window.localStorage.getItem("info")) || [];
-    storeInfo.push(savedInfo);
-    window.localStorage.setItem("info", JSON.stringify(saveInfo));
-}
+setInterval(displayTime, 1000);
